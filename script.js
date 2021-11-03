2fcf1bbf16168cdecaba21f770175b485a21b840
ameyacharnalia <ameya.charnalia@gmail.com>
2021-11-03 01:45:02 -0400

    Added footer

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const icon = document.querySelector('.icon')
 const body = document.body
 const btn = document.querySelector('.btn')
+const yearCopy = document.querySelector('.year')
 
 icon.addEventListener('click', () => {
     body.classList.toggle('light')
     btn.classList.toggle('btn-light')
 })
+
+yearCopy.innerHTML = (new Date().getFullYear());
 
